8b67a8daff272fab8f8f465e89a4b12032acc041
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-06-29 00:26:50 +0530

    Implement Number Guessing Game in Java

--- 01-NumberGuessingGame/NumberGuessingGame.java
+++ 01-NumberGuessingGame/NumberGuessingGame.java
@@ -1,7 +1,8 @@
 /**
  * NumberGuessingGame
  */
-
+//Import Required Classes
+import java.util.Scanner;
 import java.util.Random;
 
 public class NumberGuessingGame {
@@ -9,8 +10,36 @@ public class NumberGuessingGame {
     public static void main(String[] args) {
         
 Random random = new Random();
+Scanner sc = new Scanner(System.in);
 
-int numbers = random.nextInt(10);
-System.out.println(numbers);
+System.out.println("This is an number guessing game So guess my number 😏😏😏");
+
+System.out.println("Enter the range [Eg :- 10,50 ,100.....");
+int range = sc.nextInt();
+
+System.out.println("enter the number :- ");
+int yourAnswer = sc.nextInt();
+int randomNumber = random.nextInt(range);
+int attempt = 0;
+
+while (yourAnswer==randomNumber) {
+    if(yourAnswer==randomNumber){
+        System.out.println("you guess the number its "+randomNumber+"and you took "+attempt+" attempt");
+    }
+    else if (yourAnswer>randomNumber) {
+        System.out.println("your guess is too high");
+    }
+    // else if (yourAnswer<randomNumber){
+    //     System.out.println("your guess is too low");
+    // }
+    else{
+System.out.println("your guess is too low");
+    }
+    attempt++;
+}
+
+// System.out.println(randomNumber);
+
+
     }
 }
