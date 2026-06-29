c366f707da73fcad49d2b6767f3df86722c162cf
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-06-29 13:06:50 +0530

    Improve Number Guessing Game using do-while loop

--- 01-NumberGuessingGame/NumberGuessingGame.java
+++ 01-NumberGuessingGame/NumberGuessingGame.java
@@ -1,3 +1,4 @@
+
 /**
  * NumberGuessingGame
  */
@@ -8,39 +9,39 @@ import java.util.Random;
 public class NumberGuessingGame {
 
     public static void main(String[] args) {
-        
-Random random = new Random();
-Scanner sc = new Scanner(System.in);
+        // Create Scanner and Random Object
 
-System.out.println("This is an number guessing game So guess my number 😏😏😏");
+     Scanner sc = new Scanner(System.in);
+     Random  random = new Random();
+     //  Ask user to enter the range
 
-System.out.println("Enter the range [Eg :- 10,50 ,100.....");
-int range = sc.nextInt();
+     System.out.println("--- Welcome to the  number guessing game guess my number ---");
+     System.out.println("Enter the range (Eg :- 10,50,100)");
+     int range = sc.nextInt();
 
-System.out.println("enter the number :- ");
-int yourAnswer = sc.nextInt();
-int randomNumber = random.nextInt(range);
-int attempt = 0;
-int i = 0;
-while (yourAnswer!=randomNumber) {
-    if(yourAnswer==randomNumber){
-        System.out.println("you guess the number its "+randomNumber+"and you took "+attempt+" attempt");
-    }
-    else if (yourAnswer>randomNumber) {
-        System.out.println("your guess is too high");
-    }
-    // else if (yourAnswer<randomNumber){
-    //     System.out.println("your guess is too low");
-    // }
-    else{
-System.out.println("your guess is too low");
-    }
-    i++;
-    attempt++;
+     // Generate random number  
+     int randomNumber = random.nextInt(range+1);
+    int yourAnswer;
+     int attempt=0;
+     // take do while loop 
+     do{
+        // Ask user to enter guess
+        System.out.print("Enter your number lets see if you guess it or not :-  ");
+        yourAnswer = sc.nextInt();
 
-}
+        // count attempts
+        attempt++;
+        if(yourAnswer==randomNumber){
+            System.out.println(" Congratulations! your guess it and you take "+attempt+" attempts");
+        }
+        else if (yourAnswer>randomNumber) {
+            System.out.println("your guess is too high [Try again] ");
+        }
+        else if (yourAnswer<randomNumber) {
+            System.out.println("your answer is too low [Try again]");
+        }      
+     }while(yourAnswer!=randomNumber);
 
-// System.out.println(randomNumber);
 
 
     }
